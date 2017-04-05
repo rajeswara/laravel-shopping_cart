9c680c1df03158a07662792a4df3bfca5ab4f624
vigneswaran <vigneswaran.er@outlook.com>
2017-04-05 21:00:26 +0530

    complete template structure for laravel

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,14 +7,19 @@
     <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
+<link rel="stylesheet" href="{{ URL::to('src/css/app.css')}}">
 @yield('styles')
 
   </head>
   <body>
   
   @include('partials.header')
-  @yield('content')
+  <div class="container">
+    @yield('content')
+  </div>
+  
 
 <script
   src="https://code.jquery.com/jquery-3.2.1.js"
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,5 +5,101 @@
 @endsection
 
 @section('content')
-	<h1>It works !!</h1>
+	<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://www.cutlerywholesaler.com/images/products/detail/JS652_4.jpg" alt="...">
+      <div class="caption">
+        <h3> Hand Forged Samurai Sword Japanese Katana</h3>
+        <p class="description">This sword is beautiful in the very traditional sense of the samurai. The samurai found beauty in all things in nature. The blade of the sword is HQSS stainless steel, already sharpened, with the hamon along the edge of the blade.</p>
+        <div class="clearfix">
+        <div class="pull-left price">$12</div>
+        <a href="#" class="btn btn-success pull-right" role="button">Button
+        </a> 
+      </div>
+    </div>
+  </div>
+</div>
+
+ <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://www.cutlerywholesaler.com/images/products/detail/JS652_4.jpg" alt="...">
+      <div class="caption">
+        <h3> Hand Forged Samurai Sword Japanese Katana</h3>
+        <p class="description">This sword is beautiful in the very traditional sense of the samurai. The samurai found beauty in all things in nature. The blade of the sword is HQSS stainless steel, already sharpened, with the hamon along the edge of the blade.</p>
+        <div class="clearfix">
+        <div class="pull-left price">$12</div>
+        <a href="#" class="btn btn-success pull-right" role="button">Button
+        </a> 
+      </div>
+    </div>
+  </div>
+</div>
+
+
+ <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://www.cutlerywholesaler.com/images/products/detail/JS652_4.jpg" alt="...">
+      <div class="caption">
+        <h3> Hand Forged Samurai Sword Japanese Katana</h3>
+        <p class="description">This sword is beautiful in the very traditional sense of the samurai. The samurai found beauty in all things in nature. The blade of the sword is HQSS stainless steel, already sharpened, with the hamon along the edge of the blade.</p>
+        <div class="clearfix">
+        <div class="pull-left price">$12</div>
+        <a href="#" class="btn btn-success pull-right" role="button">Button
+        </a> 
+      </div>
+    </div>
+  </div>
+</div>
+
+ <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://www.cutlerywholesaler.com/images/products/detail/JS652_4.jpg" alt="...">
+      <div class="caption">
+        <h3> Hand Forged Samurai Sword Japanese Katana</h3>
+        <p class="description">This sword is beautiful in the very traditional sense of the samurai. The samurai found beauty in all things in nature. The blade of the sword is HQSS stainless steel, already sharpened, with the hamon along the edge of the blade.</p>
+        <div class="clearfix">
+        <div class="pull-left price">$12</div>
+        <a href="#" class="btn btn-success pull-right" role="button">Button
+        </a> 
+      </div>
+    </div>
+  </div>
+</div>
+
+
+ <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://www.cutlerywholesaler.com/images/products/detail/JS652_4.jpg" alt="...">
+      <div class="caption">
+        <h3> Hand Forged Samurai Sword Japanese Katana</h3>
+        <p class="description">This sword is beautiful in the very traditional sense of the samurai. The samurai found beauty in all things in nature. The blade of the sword is HQSS stainless steel, already sharpened, with the hamon along the edge of the blade.</p>
+        <div class="clearfix">
+        <div class="pull-left price">$12</div>
+        <a href="#" class="btn btn-success pull-right" role="button">Button
+        </a> 
+      </div>
+    </div>
+  </div>
+</div>
+
+
+ <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://www.cutlerywholesaler.com/images/products/detail/JS652_4.jpg" alt="...">
+      <div class="caption">
+        <h3> Hand Forged Samurai Sword Japanese Katana</h3>
+        <p class="description">This sword is beautiful in the very traditional sense of the samurai. The samurai found beauty in all things in nature. The blade of the sword is HQSS stainless steel, already sharpened, with the hamon along the edge of the blade.</p>
+        <div class="clearfix">
+        <div class="pull-left price">$12</div>
+        <a href="#" class="btn btn-success pull-right" role="button">Button
+        </a> 
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</div>
+
 @endsection
