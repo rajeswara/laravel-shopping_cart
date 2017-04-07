32a79ffc9c30f3504b210003fc1118f3b5fa59d2
vigneswaran <vigneswaran.er@outlook.com>
2017-04-07 21:23:38 +0530

    Stripe Credit Card Verification

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -9,7 +9,11 @@
 				<div class="col-sm-6 col-md-4 col-md-offset-4 col-sm-offset-3">
 					<h1>Checkout</h1>
 					<h4>Your Total: ${{ $total }}</h4>
+					<div id="charge-error" class="alert alert-danger {{ !Session::has('error') ? 'hidden' : '' }}">
 
+					{{ Session::get('error') }}
+						
+					</div>
 					<form action=" {{ route('checkout')}}" method="post" id="checkout-form">
 						
 						<div class="row">
@@ -96,4 +100,11 @@
 					</form>
 				</div>
 			</div>
+@endsection
+
+
+@section('scripts')
+		<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+		<script type="text/javascript" src="{{ URL::to('src/js/checkout.js') }}"></script>
+		
 @endsection
