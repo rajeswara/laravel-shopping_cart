1fa238236b184e2772b1ac8e34246d714361c6d5
vigneswaran <vigneswaran.er@outlook.com>
2017-04-08 20:14:08 +0530

    displaying orders in user profile :)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -71,7 +71,12 @@ class UserController extends Controller
     }
 
     public function getProfile(){
-    	return view('user.profile');
+        $orders = Auth::user()->orders;
+        $orders->transform(function($order, $key){
+            $order->cart = unserialize($order->cart);
+            return $order;
+        });
+    	return view('user.profile',['orders'=>$orders]);
     }
 
     public function getLogout(){
